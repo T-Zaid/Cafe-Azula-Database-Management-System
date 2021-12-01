c440098131449144cf90a3c4fdb0848d18e5bcf6
T Zaid <ZaidBShahab@gmail.com>
2021-12-01 22:27:11 +0500

    WinForm initilization
    Login Page created

---
+++ Azula Cafe Database Management System/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Azula_Cafe_Database_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.LoginPage = new System.Windows.Forms.TabPage();
+            this.Register = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AccountPassword = new System.Windows.Forms.TextBox();
+            this.AccountName = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.LoginPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.LoginPage);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-7, -30);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(814, 493);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // LoginPage
+            // 
+            this.LoginPage.Controls.Add(this.Register);
+            this.LoginPage.Controls.Add(this.label4);
+            this.LoginPage.Controls.Add(this.LoginButton);
+            this.LoginPage.Controls.Add(this.label3);
+            this.LoginPage.Controls.Add(this.label2);
+            this.LoginPage.Controls.Add(this.label1);
+            this.LoginPage.Controls.Add(this.AccountPassword);
+            this.LoginPage.Controls.Add(this.AccountName);
+            this.LoginPage.Location = new System.Drawing.Point(4, 22);
+            this.LoginPage.Name = "LoginPage";
+            this.LoginPage.Padding = new System.Windows.Forms.Padding(3);
+            this.LoginPage.Size = new System.Drawing.Size(806, 467);
+            this.LoginPage.TabIndex = 0;
+            this.LoginPage.Text = "Login Page";
+            this.LoginPage.UseVisualStyleBackColor = true;
+            // 
+            // Register
+            // 
+            this.Register.Location = new System.Drawing.Point(359, 334);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(75, 23);
+            this.Register.TabIndex = 7;
+            this.Register.Text = "Register";
+            this.Register.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(356, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Not a user ?";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(359, 275);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(222, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter Password :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Username :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(178, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(411, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Azula Cafe Management System";
+            // 
+            // AccountPassword
+            // 
+            this.AccountPassword.Location = new System.Drawing.Point(337, 214);
+            this.AccountPassword.Name = "AccountPassword";
+            this.AccountPassword.Size = new System.Drawing.Size(123, 20);
+            this.AccountPassword.TabIndex = 1;
+            // 
+            // AccountName
+            // 
+            this.AccountName.Location = new System.Drawing.Point(336, 154);
+            this.AccountName.Name = "AccountName";
+            this.AccountName.Size = new System.Drawing.Size(124, 20);
+            this.AccountName.TabIndex = 0;
+            this.AccountName.TextChanged += new System.EventHandler(this.AccountName_TextChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(806, 467);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Welcome";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.LoginPage.ResumeLayout(false);
+            this.LoginPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage LoginPage;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AccountPassword;
+        private System.Windows.Forms.TextBox AccountName;
+    }
+}
+
---
+++ Azula Cafe Database Management System/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Azula_Cafe_Database_Management_System
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+            string connetionString = @"Data Source=ZAID-PC\SERWORK;Initial Catalog=TigerMedia;Integrated Security = True";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+        }
+
+        private void AccountName_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (cnn.State == System.Data.ConnectionState.Open)
+            {
+                if (AccountName.Text.Length > 0 && AccountPassword.Text.Length > 0)
+                {
+                    string sql = "select * from Accounts where Username = '" + AccountName.Text.ToString() + "' and AccPwd = '" + AccountPassword.Text.ToString() + "'";
+                    cmd = new SqlCommand(sql, cnn);
+                    reader = cmd.ExecuteReader();
+                    if (reader.Read()) //if the account exists
+                    {
+                            int AccNo = Convert.ToInt32(reader["AccountNo"]);
+                            reader.Close();
+                            cmd.Dispose();
+                            tabControl1.SelectedTab = tabPage2;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        cmd.Dispose();
+                        MessageBox.Show("Invalid Credentials");
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Debug Error 1 : Database not connected");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cnn.Close();
+        }
+    }
+}
