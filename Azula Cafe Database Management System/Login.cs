6510dde6a913c2a6c389a869a976eb07828c03de
T Zaid <ZaidBShahab@gmail.com>
2021-12-04 16:18:34 +0500

    Added Customer and Staff Account Creation functions
    Only the code is added fn, the design will be added soon

--- Azula Cafe Database Management System/Login.cs
+++ Azula Cafe Database Management System/Login.cs
@@ -60,9 +60,62 @@ namespace Azula_Cafe_Database_Management_System
             return Staff_or_Cust;
         }
 
+        public int CustomerAccountCreate(string Name, string phone, string username, string password)
+        {
+            string sql = "select * from Accounts where Username = " + username;
+            cmd = new SqlCommand(sql, cnn);
+            reader = cmd.ExecuteReader();
+            int existingUserName = (reader.Read()) ? 1 : 0; //1 if the account with the specific username exists, 0 if not
+            if (existingUserName != 0)
+            {
+                string sql1 = "select max(AccountNo) from Accounts", sql2 = "select max(CustomerID) from Customers";
+                cmd = new SqlCommand(sql1, cnn);
+                int MaxAcc = Convert.ToInt32(cmd.ExecuteScalar());
+                cmd = new SqlCommand(sql2, cnn);
+                int MaxCum = Convert.ToInt32(cmd.ExecuteScalar());
+
+                sql1 = "Insert into Accounts (AccountNo, Username, AccPassword) values (" + (MaxAcc + 1) + ", '" + username + "', '" + password + "')";
+                sql2 = "Insert into Customers (CustomerID, CustName, PhoneNo, AccountNo) values (" + (MaxCum + 1) + ", '" + Name + "', '" + phone + "', " + (MaxAcc + 1) + ")";
+                cmd = new SqlCommand(sql1, cnn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(sql2, cnn);
+                cmd.ExecuteNonQuery();
+                return 1;
+
+            }
+            return 0;
+        }
+
+        public int StaffAccountCreate(string Name, string username, string password, string phone, string Position, string salary)
+        {
+            string sql = "select * from Accounts where Username = " + username;
+            cmd = new SqlCommand(sql, cnn);
+            reader = cmd.ExecuteReader();
+            int existingUserName = (reader.Read()) ? 1 : 0; //1 if the account with the specific username exists, 0 if not
+            if (existingUserName != 0)
+            {
+                string sql1 = "select max(AccountNo) from Accounts", sql2 = "select max(StaffID) from Staff";
+                cmd = new SqlCommand(sql1, cnn);
+                int MaxAcc = Convert.ToInt32(cmd.ExecuteScalar());
+                cmd = new SqlCommand(sql2, cnn);
+                int MaxStf = Convert.ToInt32(cmd.ExecuteScalar());
+
+                sql1 = "Insert into Accounts (AccountNo, Username, AccPassword) values (" + (MaxAcc + 1) + ", '" + username + "', '" + password + "')";
+                sql2 = "Insert into Staff (StaffID, StaffName, PhoneNo, Salary, AccountNo, Position) values (" + (MaxStf + 1) + ", '" + Name + "', '" + phone + "', " + salary + ", " + (MaxAcc + 1) + ", " + Position + ")";
+                cmd = new SqlCommand(sql1, cnn);
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand(sql2, cnn);
+                cmd.ExecuteNonQuery();
+                return 1;
+
+            }
+            return 0;
+        }
+
         ~Login()
         {
-            reader.Close();
+            if(reader.HasRows)  //if it wasn't open, there's no point in closing it...
+                reader.Close();
             cmd.Dispose();
             cnn.Close();
         }
